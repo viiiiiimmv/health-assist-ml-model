cd17752789c75c5d9f3d58afa25734b27fcd6bbb
Shiv Chauhan <shiv.chauhan1518@gmail.com>
2025-08-25 14:20:49 +0530

    requirements changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="health-assist",
+    version="0.1.0",
+    author="Shiv Chauhan",
+    author_email="shivchauhan41223@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    )
