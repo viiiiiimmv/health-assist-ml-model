b51107d365ea2d74d4dd46932cb9492718e50c88
Shiv Chauhan <shiv.chauhan1518@gmail.com>
2025-09-19 14:59:57 +0530

    updates_v1

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# * youtube video link : https://www.youtube.com/live/KnoVFU0yCUc?si=M8SQ9V-cfEWLZOZs
-
 from flask import Flask, render_template, jsonify, request
 from src.helper import download_hugging_face_embeddings
 from langchain_pinecone import PineconeVectorStore
@@ -14,23 +12,22 @@ import os
 app = Flask(__name__)
 load_dotenv()
 
-PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
-OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
 os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
-
 embeddings = download_hugging_face_embeddings()
 
-index_name = "health-assist" 
-# Embed each chunk and upsert the embeddings into your Pinecone index.
+index_name = "health-assist"
+
 docsearch = PineconeVectorStore.from_existing_index(
     index_name=index_name,
     embedding=embeddings
 )
 
-retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
+retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k": 3})
 
 chatModel = ChatOpenAI(model="gpt-5-mini")
 prompt = ChatPromptTemplate.from_messages(
@@ -38,24 +35,29 @@ prompt = ChatPromptTemplate.from_messages(
         ("system", system_prompt),
         ("human", "{input}"),
     ]
-)
+) 
 
 question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
 rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
 
 @app.route("/")
 def index():
     return render_template('chat.html')
 
-@app.route("/get", methods=["GET", "POST"])
+
+@app.route("/get", methods=["POST"])
 def chat():
-    msg = request.form["msg"]
-    input = msg
-    print(input)
+    msg = request.form.get("msg") or request.json.get("text")
+    if not msg:
+        return jsonify({"error": "Message is required"}), 400
+
+    print("User Input:", msg)
     response = rag_chain.invoke({"input": msg})
-    print("Response : ", response["answer"])
-    return str(response["answer"])
+    print("Response:", response["answer"])
+
+    return jsonify({"response": response["answer"]})
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 8080, debug= True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
